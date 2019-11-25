eea82099055138e00862ae1f9d5714ca04a3510c
Steven Maude <git@stevenmaude.co.uk>
2019-11-25 15:44:58 +0000

    Update version in setup.py
    Backwards compatible new feature added.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ requires=['pelican>=3.6.0']
 
 setup(
     name="tag_cloud",
-    version="0.0.1",
+    version="0.1.0",
     url='https://github.com/getpelican/pelican-plugins',
     description="A tool to generate Pelican tag clouds.",
     py_modules=['tag_cloud'],
